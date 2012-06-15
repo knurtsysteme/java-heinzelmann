e3452bd51fe1978818a0ab9fc3c30d07afe354ff
Daniel Oltmanns <info@knurt.de>
2012-06-15 08:59:52 +0200

    add a abstract ValueConverter

---
+++ hm-util/src/main/java/de/knurt/heinzelmann/util/adapter/ValueConverter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2005 - 2009 by KNURT Systeme (http://www.knurt.de)
+ *
+ * Licensed under the Creative Commons License Attribution-NonCommercial 3.0 Unported;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://creativecommons.org/licenses/by-nc/3.0/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.knurt.heinzelmann.util.adapter;
+
+/**
+ * convert values to something else
+ * 
+ * @since 06/15/2012
+ * @author danieloltmanns@knurt.de
+ */
+public abstract class ValueConverter<E> {
+	private E[] converted;
+	private E[] original;
+
+	public ValueConverter(E[] values) {
+		this.original = values;
+		this.converted = values.clone();
+		for (int i = 0; i < values.length; i++) {
+			this.converted[i] = this.convert(values[i]);
+		}
+	}
+
+	protected abstract E convert(E value);
+
+	public E[] original() {
+		return original;
+	}
+
+	public E original(int i) {
+		return original[i];
+	}
+
+	public int length() {
+		return original.length;
+	}
+
+	public E[] converted() {
+		return converted;
+	}
+
+	public E converted(int i) {
+		return converted[i];
+	}
+}
